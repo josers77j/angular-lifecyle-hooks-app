0c666fbfd986c480cc54bc9a25519b205fffe7eb
josers77j <josers77.1@gmail.com>
2025-04-12 14:47:54 -0600

    feat: pages configuration

---
+++ src/app/pages/about-page/about-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-page',
+  imports: [],
+  templateUrl: './about-page.component.html',
+
+})
+export class AboutPageComponent { }
